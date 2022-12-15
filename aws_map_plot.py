44aa182b4c3d89c7277f40caf4d449e137332646
Adithya Guduru <adithyag123@gmail.com>
2022-12-15 12:52:49 -0500

    Added user controller files

---
+++ aws_map_plot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+with open('center_points.txt') as f:
+    lines = f.readlines()
+    xlist = []
+    ylist = []
+    for num, loc in enumerate(lines):
+        pos = loc.split(' ')[1][:-1]
+        if num%3 == 0:
+            xlist.append(pos)
+        elif num%3 == 1:
+            ylist.append(pos)
+    plt.plot(xlist, ylist)
+    plt.show()
